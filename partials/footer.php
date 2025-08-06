1cc47bc36874209a252f86c90bd54dca314f9e37
Mohit Sharma <118476019+xelliann@users.noreply.github.com>
2025-08-06 14:25:47 +0530

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -12,20 +12,20 @@
     <div class="footer-section">
       <h4>Support</h4>
       <ul>
-        <li><a href="<?=ROOT_URL?>help.php">Online Help</a></li>
-        <li><a href="<?=ROOT_URL?>contact_us.php">Call Us</a></li>
-        <li><a href="<?=ROOT_URL?>contact_us.php">Email</a></li>
-        <li><a href="<?=ROOT_URL?>help.php">FAQ</a></li>
+        <li><a href="../help.php">Online Help</a></li>
+        <li><a href="../contact_us.php">Call Us</a></li>
+        <li><a href="../contact_us.php">Email</a></li>
+        <li><a href="../help.php">FAQ</a></li>
       </ul>
     </div>
     <div class="footer-section">
       <h4>Quick Links</h4>
       <ul>
-        <li><a href="<?=ROOT_URL?>index.php">Home</a></li>
-        <li><a href="<?=ROOT_URL?>exercise_plans.php">Exercise Plans</a></li>
-        <li><a href="<?=ROOT_URL?>diet_plans.php">Diet Plans</a></li>
-        <li><a href="<?=ROOT_URL?>about_us.php">About</a></li>
-        <li><a href="<?=ROOT_URL?>contact_us.php">Contact</a></li>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../exercise_plans.php">Exercise Plans</a></li>
+        <li><a href="../diet_plans.php">Diet Plans</a></li>
+        <li><a href="../about_us.php">About</a></li>
+        <li><a href="../contact_us.php">Contact</a></li>
       </ul>
     </div>
     <!-- <div class="footer-section">
