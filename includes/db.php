c69cf26343a4f5e83b45a1fe1174264292682ba7
Mohit Sharma <118476019+xelliann@users.noreply.github.com>
2025-08-21 14:27:28 +0530

    Delete includes directory

--- includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-$conn = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
-
-if ($conn->connect_error) {
-    die("❌ Connection failed: " . $conn->connect_error);
-}
-
-?>
